804cd8e007abc2cd7f882eeda11efcd208f0994a
midhun-sudhakar <midhun@zeefasys.com>
2019-08-28 17:16:22 +0530

    Configuring environments

--- src/config.js
+++ src/config.js
@@ -1,17 +1,44 @@
-export default {
-  MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
   s3: {
-    REGION: "us-east-1",
-    BUCKET: "notes-app-uploads"
+    REGION: "YOUR_DEV_S3_UPLOADS_BUCKET_REGION",
+    BUCKET: "YOUR_DEV_S3_UPLOADS_BUCKET_NAME"
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://5by75p4gn3.execute-api.us-east-1.amazonaws.com/prod"
+    URL: "https://api.zeefasys.net/dev"
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_udmFFSb92",
-    APP_CLIENT_ID: "4hmari2sqvskrup67crkqa4rmo",
-    IDENTITY_POOL_ID: "us-east-1:ceef8ccc-0a19-4616-9067-854dc69c2d82"
+    USER_POOL_ID: "us-east-1_b1gxedlnK",
+    APP_CLIENT_ID: "4d57aumb7rjpid22k15bpmbg7q",
+    IDENTITY_POOL_ID: "us-east-1:5ecb5ccf-1cbb-42e3-a491-79b92d08f012"
   }
+};
+
+const prod = {
+  s3: {
+    REGION: "YOUR_PROD_S3_UPLOADS_BUCKET_REGION",
+    BUCKET: "YOUR_PROD_S3_UPLOADS_BUCKET_NAME"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://api.zeefasys.net/dev"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_yaE87jFnu",
+    APP_CLIENT_ID: "1pf9oc1b9trqlk46088egk9c6b",
+    IDENTITY_POOL_ID: "us-east-1:d13b9c2f-113b-487a-8333-08e2295e5eeb"
+  }
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod'
+  ? prod
+  : dev;
+
+export default {
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
 };
